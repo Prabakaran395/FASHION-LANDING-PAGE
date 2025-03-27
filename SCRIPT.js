1f42ef70c8e607b5720fb6152640c4e88c89893b
prabakaran395 <prabakaran7936@gmail.com>
2025-03-27 21:44:08 +0530

    first commit

---
+++ SCRIPT.js
@@ -0,0 +1,15 @@
+// Open Login Modal
+function openModal() {
+  document.getElementById("loginModal").style.display = "flex";
+}
+
+// Close Login Modal
+function closeModal() {
+  document.getElementById("loginModal").style.display = "flex";
+}
+
+// Toggle Mobile Menu
+function toggleMenu() {
+  var menu = document.getElementById("nav-links");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
